8ba53a522d6162fe6094d5ddb0e8779efa12c135
shaheryar abid <sharyar0310@gmail.com>
2023-05-16 19:54:40 +0500

    Change initialState to an empty Array

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const categoriesSlice = createSlice({
   name: 'categories',
-  initialState: 'Under construction',
+  initialState: [],
   reducers: {
     checkStatus: () => 'Under construction',
   },
