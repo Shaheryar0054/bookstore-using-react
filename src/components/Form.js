91fb3f8cf8182fe4c86edf57ee7537465bc9e65c
shaheryar abid <sharyar0310@gmail.com>
2023-05-18 18:43:20 +0500

    Connected bookstore with Api

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,35 +1,58 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/booksSlice';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addBook, postBooks } from '../redux/books/booksSlice';
 
-export default function Form() {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
+const Form = () => {
+  const initial = {
+    title: '',
+    author: '',
+  };
 
+  const [state, setState] = useState(initial);
+  const { books } = useSelector((state) => state.books);
   const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (title.trim() !== '' && author.trim() !== '') {
-      dispatch(addBook({ title, author }));
-      setTitle('');
-      setAuthor('');
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prev) => ({
+      item_id: `item${books.length + 1}`, ...prev, [name]: value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    if (state.title.trim() !== '' && state.author.trim() !== '') {
+      const book = {
+        title: state.title,
+        author: state.author,
+        category: 'Story',
+        item_id: `item${books.length + 1}`,
+      };
+
+      try {
+        await dispatch(postBooks(book));
+        dispatch(addBook(book));
+        setState(initial);
+      } catch (error) {
+        error(error);
+      }
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="title">
-        Title:
-        <input placeholder="Title" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
-      </label>
-
-      <label htmlFor="author">
-        Author:
-        <input placeholder="authore" id="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
-      </label>
-
-      <button type="submit">Add Book</button>
-    </form>
+    <>
+      <h3>ADD NEW BOOK</h3>
+      <form action="submit">
+        <input type="text" placeholder="Book title" onChange={handleChange} name="title" value={state.title} />
+        <input type="text" placeholder="Author" onChange={handleChange} name="author" value={state.author} />
+        <button
+          type="button"
+          onClick={handleSubmit}
+        >
+          Add Book
+        </button>
+      </form>
+    </>
   );
-}
+};
+
+export default Form;
